d34b8c7f008b481e913ca5e1b9c6d1b6205aa634
Sachin Verma <sachinvermarip@gmail.com>
2014-06-29 15:31:58 +0530

    Fixed color picker
    Color picker JS loads only on widgets page now. Now also works after
    saving the settings.

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,16 +1,15 @@
-// Common function to do the Trick and our jQuery action
-function test_click() {
+// Common function to do the trick and our jQuery action
+function color_picker_load() {
     var number = 1 + Math.floor( Math.random() * 5000000 );
-    jQuery( this ).html( 'WordPress Answers #' + number );
+    jQuery( this ).html( 'Color Picker' + number );
 }
 
 // Our jQuery
 jQuery( document ).ready( function( $ ) {
-    $( '.test-me' ).click( test_click );
+    jQuery('.color-picker').wpColorPicker();
 });
 
-// OneTrick's
-// http://wordpress.stackexchange.com/a/37707
+// Load jquery on save
 jQuery( document ).ajaxComplete( function( event, XMLHttpRequest, ajaxOptions ) {
     var request = {}, pairs = ajaxOptions.data.split('&'), i, split, widget;
     for( i in pairs ) {
@@ -23,6 +22,6 @@ jQuery( document ).ajaxComplete( function( event, XMLHttpRequest, ajaxOptions ) 
         if( !XMLHttpRequest.responseText ) 
             wpWidgets.save(widget, 0, 1, 0);
         else
-            jQuery( '.color-picker').wpColorPicker();
+            jQuery('.color-picker').wpColorPicker();
     }
 });
